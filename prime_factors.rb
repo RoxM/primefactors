d88a87bd266a7944108b3f545cc8ac98aa0821b9
roxm <rossymr@gmail.com>
2015-03-29 22:27:21 -0600

    PrimeFactors(8) Green
    Pass with the WHILE for IF Transformation

--- prime_factors.rb
+++ prime_factors.rb
@@ -2,7 +2,7 @@ module PrimeFactors
 	def self.of(n)
 		factorList = []
 		if n>1 
-			factorList << 2 and n/=2 if n%2 == 0
+			factorList << 2 and n/=2 while n%2 == 0
 			factorList << n if n>1 
 		end
 		return factorList
