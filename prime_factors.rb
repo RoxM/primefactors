6277ad3701ff01739fbd7426496580c8a242e651
roxm <rossymr@gmail.com>
2015-03-29 22:13:46 -0600

    PrimeFactors(4) Green
    uses /= to divide and change n for the second (2,2)

--- prime_factors.rb
+++ prime_factors.rb
@@ -2,7 +2,11 @@ module PrimeFactors
 	def self.of(n)
 		factorList = []
 		if n>1 
-			factorList << n 
+			if n%2 == 0
+				factorList << 2
+				n /= 2
+			end
+			factorList << n if n>1 
 		end
 		return factorList
 	end
