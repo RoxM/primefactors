afabf2097119e8d43be951c7605f385bd41626ce
roxm <rossymr@gmail.com>
2015-03-29 21:29:08 -0600

    PrimeFactor(2) RED
    Test with 2 Added

--- spec/prime_factor_spec.rb
+++ spec/prime_factor_spec.rb
@@ -1,7 +1,11 @@
 require_relative '../prime_factors'
 
 describe PrimeFactors do
-	it 'nothing is return for 1' do
+	it 'nothing is returned for 1' do
 		expect(PrimeFactors.of(1)).to eq []
+	end
+	
+	it '2 is returned for 2' do
+		expect(PrimeFactors.of(2)).to eq [2]
 	end
 end
