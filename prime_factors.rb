b093532e726b2a93a85cb8dc8ac42c66ca32f085
roxm <rossymr@gmail.com>
2015-03-29 22:16:39 -0600

    PrimeFactors (4) refactor
    Change the if for the first 2 to one line :)

--- prime_factors.rb
+++ prime_factors.rb
@@ -2,10 +2,7 @@ module PrimeFactors
 	def self.of(n)
 		factorList = []
 		if n>1 
-			if n%2 == 0
-				factorList << 2
-				n /= 2
-			end
+			factorList << 2 and n/=2 if n%2 == 0
 			factorList << n if n>1 
 		end
 		return factorList
