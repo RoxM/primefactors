07cf84da16989bee02f373ee80f18f4e63a56d92
roxm <rossymr@gmail.com>
2015-03-29 22:48:48 -0600

    PrimeFactors(9) refactor
    Delete the line factorList << n if n>1 because with the while was not
    needed.
    
    Put the divisor increment with the WHILE

--- prime_factors.rb
+++ prime_factors.rb
@@ -1,12 +1,10 @@
 module PrimeFactors
 	def self.of(n)
 		factorList = []
-		divisor = 2
-		while n>1 
+		divisor = 1
+		while n>1 and divisor += 1
 			factorList << divisor and n/=divisor while n%divisor == 0
-			divisor += 1
 		end
-		factorList << n if n>1
 		return factorList
 	end
 end
